e709ede3cfca8e762e5075c29432cf22380cf5f2
Saran Raj <141270926+saranraj023@users.noreply.github.com>
2024-02-10 22:20:00 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cicdproj" # Replace with your actual S3 bucket name
+    bucket = "cicdproj" 
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
